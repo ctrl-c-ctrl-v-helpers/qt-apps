12fa7c5c564a092b68e22dcb3ec11f9b823ad6ca
Piotr Mika <piotr.mt.mika@gmail.com>
2026-08-10 21:08:26 +0200

    local deploy with app restart

--- menu-stop/deploy.sh
+++ menu-stop/deploy.sh
@@ -1,20 +1,37 @@
 #!/bin/bash
 
+NSTEPS=5
+
 if ! [ -z "$(git status --porcelain)" ]; then
 	echo Cannot deploy dirty repo build.
-	#exit 1
+	exit 1
 fi
 
 SRC=$( find . -name menu-stop.exe )
 TXT=$( git log -1 --format=menu-stop---%cd_%s_%h---exe.txt --date=format:%Y-%m-%dT%H-%M HEAD | sed 's/ /-/g' )
 DST='/c/EXE/qt-apps/menu-stop.exe'
 
-echo cp $SRC $TXT
-cp -v $SRC $TXT
+echo "[1/$NSTEPS] Stopping menu-stop.exe:"
+echo -n "      "
+taskkill //f //im menu-stop.exe
+echo
+
+echo "[2/$NSTEPS] Copying sendable version:"
+echo "      copy from $SRC"
+echo "      copy to   $TXT"
+cp $SRC $TXT
+echo
+
+echo "[3/$NSTEPS] Copying local version:"
+echo "      copy from $SRC"
+echo "      copy to   $DST"
+cp $SRC $DST
+echo
+
+echo "[4/$NSTEPS] Starting menu-stop.exe:"
+cmd.exe //c "cd /d C:\EXE\qt-apps && start menu-stop.exe"
+tasklist | grep -E "Image Name|=====|menu-stop.exe" | awk '{print "      " $0}'
+
 
 echo
-echo cp $SRC $DST
-cp -v $SRC $DST
-
-echo
-echo DONE
+echo "[5/$NSTEPS] DONE"
